cb599d9984c0cf2aa41938bc6696d76cb22d491f
jehanp <jehanp@zone24x7.com>
2020-11-11 17:29:47 +0530

    Add create task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { TaskRepository } from './task.repository';
 
@@ -46,19 +47,9 @@ export class TasksService {
         return found;
     }
 
-    // createTask(createTaskDto: CreateTaskDto): Task{
-    //     const {title, description} = createTaskDto;
-
-    //     const task:Task = {
-    //         id: uuid.v1(),
-    //         title,
-    //         description,
-    //         status: TaskStatus.OPEN
-    //     };
-
-    //     this.tasks.push(task);
-    //     return task;
-    // }
+    async createTask(createTaskDto: CreateTaskDto):Promise<Task>{
+        return this.taskRepository.createTask(createTaskDto);
+    }
 
     // updateTaskStatus(id:string, status:TaskStatus):Task{
     //     const ids = id;
