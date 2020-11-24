d6755faf6664aa93957e20f88682ee7a09139fd9
jehanp <jehanp@zone24x7.com>
2020-11-24 13:45:20 +0530

    Add html file

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  console.log(process.env.NODE_ENV);
   if(process.env.NODE_ENV === 'development'){
     app.enableCors();
   }
